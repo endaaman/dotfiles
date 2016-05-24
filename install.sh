c5ebbe2eee930a8f6f379f890fe0c7e7f8e75d4e
endaaman <buhibuhidog@gmail.com>
2016-05-24 17:05:35 +0900

    add vimperatorrc

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ ln -fs ~/dotfiles/gitconfig ~/.gitconfig
 ln -fs ~/dotfiles/gitignore_global ~/.gitignore_global
 ln -fs ~/dotfiles/zshrc ~/.zshrc
 ln -fs ~/dotfiles/vimrc ~/.vimrc
+ln -fs ~/dotfiles/vimperatorrc ~/.vimperatorrc
 
 mkdir -p ~/.vim
 
