fbd8d1cf7e6a6c293d40a75783976ef04da98dd2
endaaman <buhibuhidog@gmail.com>
2016-06-20 17:16:14 +0900

    separate link script

--- install.sh
+++ install.sh
@@ -4,14 +4,6 @@ CYAN='\033[0;34m'
 NC='\033[0m'
 
 cd
-
-ln -fs ~/dotfiles/editorconfig ~/.editorconfig
-ln -fs ~/dotfiles/gitconfig ~/.gitconfig
-ln -fs ~/dotfiles/gitignore_global ~/.gitignore_global
-ln -fs ~/dotfiles/zshrc ~/.zshrc
-ln -fs ~/dotfiles/vimrc ~/.vimrc
-ln -fs ~/dotfiles/vimperatorrc ~/.vimperatorrc
-
 mkdir -p ~/.vim
 
 
---
+++ link.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -fs ~/dotfiles/editorconfig ~/.editorconfig
+ln -fs ~/dotfiles/gitconfig ~/.gitconfig
+ln -fs ~/dotfiles/gitignore_global ~/.gitignore_global
+ln -fs ~/dotfiles/zshrc ~/.zshrc
+ln -fs ~/dotfiles/vimrc ~/.vimrc
+ln -fs ~/dotfiles/vimperatorrc ~/.vimperatorrc
+
