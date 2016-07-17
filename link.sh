ddb7f6ed297b1a95a2e772f46affa8439da1b354
endaaman <buhibuhidog@gmail.com>
2016-07-17 23:49:24 +0900

    update

--- link.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ln -fs ~/dotfiles/editorconfig ~/.editorconfig
-ln -fs ~/dotfiles/gitconfig ~/.gitconfig
-ln -fs ~/dotfiles/gitignore_global ~/.gitignore_global
-ln -fs ~/dotfiles/zshrc ~/.zshrc
-ln -fs ~/dotfiles/vimrc ~/.vimrc
-ln -fs ~/dotfiles/vimperatorrc ~/.vimperatorrc
-
