efc9f95c965ff9ea971952dac267476cf5667c2f
endaaman <endaaman@eis.hokudai.ac.jp>
2016-12-13 01:59:04 +0900

    update completion

--- zsh/functions/_lxd.zsh
+++ zsh/functions/_lxd.zsh
@@ -25,11 +25,23 @@ _lxc() {
                 _arguments -C \
                   '2: :__lxc_config_device_commands' \
                   && ret=0
+                case $words[3] in
+                  (add|get|set|unset|list|show|remove)
+                    _arguments -C \
+                      '3: :__lxc_containers_all' \
+                      && ret=0
+                    ;;
+                esac
                 ;;
               (trust)
                 _arguments -C \
                   '2: :__lxc_config_trust_commands' \
                   && ret=0
+                case $words[3] in
+                  (list|add|remove)
+                    # complete remote
+                    ;;
+                esac
                 ;;
             esac
           ;;
@@ -59,17 +71,26 @@ _lxc() {
         (file)
           _arguments -C \
             {--help,--help}'[Show help]' \
+            '1: :__lxc_file_commands' \
             && ret=0
           ;;
         (help)
           _arguments -C \
             {--help,--help}'[Show help]' \
+          '1: :__lxc_commands' \
             && ret=0
           ;;
         (image)
           _arguments -C \
             {--help,--help}'[Show help]' \
+          '1: :__lxc_image_commands' \
             && ret=0
+            case $words[2] in
+              (alias)
+                _arguments -C \
+                  '2: :__lxc_image_alias_commands' \
+                  && ret=0
+                ;;
           ;;
         (info)
           _arguments -C \
@@ -217,10 +238,45 @@ __lxc_config_trust_commands() {
     _describe -t config_trust_commands 'Config trust commands' _c
 }
 
+__lxc_file_commands() {
+    local -a _c
+    _c=(
+      'pull:Pull file from container'
+      'push:Push file to container'
+      'edit:Edit file on container'
+    )
+    _describe -t file_commands 'File commands' _c
+}
+
+__lxc_image_commands() {
+    local -a _c
+    _c=(
+      'copy:Copy an image from one LXD daemon to another over the network'
+      'delete:Delete one or more images from the LXD image store'
+      'export:Export an image from the LXD image store into a distributable tarball'
+      'info:Print everything LXD knows about a given image'
+      'list:List images in the LXD image store. Filters may be of the'
+      'show:Yaml output of the user modifiable properties of an image'
+      'edit:Edit image, either by launching external editor or reading STDIN'
+      'alias:Manage alias for image in the LXD image store'
+    )
+    _describe -t image_commands 'Image commands' _c
+}
+
+
+__lxc_file_commands() {
+    local -a _c
+    _c=(
+      'pull:Pull file from container'
+      'push:Push file to container'
+      'edit:Edit file on container'
+    )
+    _describe -t file_commands 'Image alias commands' _c
+}
+
 __lxc_containers_running () {
     local -a _containers
     _containers=(${(@f)"$(_call_program repositories lxc list --fast | tail -n +4 | awk '{print $2 $4}' | egrep 'RUNNING$' | sed -e "s/RUNNING//")"})
-    # _containers=$(lxc list --fast | tail -n +4 | awk '{print $2 $4}' | egrep 'RUNNING$' | sed -e "s/RUNNING//")
     _describe -t containers 'Running Containers' _containers
 }
 
@@ -232,8 +288,10 @@ __lxc_containers_stopped () {
 
 __lxc_containers_all () {
     local -a _containers
-    _containers=(${(@f)"$(_call_program repositories lxc list --fast | tail -n +4 | awk '{print $2 $4}' | egrep -v '^(\||^$)')"})
+    _containers=(${(@f)"$(_call_program repositories lxc list --fast | tail -n +4 | awk '{print $2}' | egrep -v '^(\||^$)')"})
     _describe -t containers 'All containers' _containers
 }
 
 compdef _lxc lxc
+
+
