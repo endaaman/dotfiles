efe455763d5daa66ae292e03211791e1d6a31812
endaaman <buhibuhidog@gmail.com>
2017-07-19 18:18:26 +0900

    update tym color

--- tym.lua
+++ tym.lua
@@ -13,14 +13,14 @@ config.color_cursor            = '#d0d0d0'
 config.color_cursor_foreground = '#181818'
 config.color_background        = '#181818'
 
-config.color_0  = '#181818' -- overwritten by color_foreground
+config.color_0  = '#181818' -- overwritten by color_background
 config.color_1  = '#ac4142'
 config.color_2  = '#90a959'
 config.color_3  = '#f4bf75'
 config.color_4  = '#6a9fb5'
 config.color_5  = '#aa759f'
 config.color_6  = '#75b5aa'
-config.color_7  = '#d0d0d0' -- overwritten by color_background
+config.color_7  = '#d0d0d0' -- overwritten by color_foreground
 config.color_8  = '#505050'
 config.color_9  = '#ac4142'
 config.color_10 = '#90a959'
@@ -29,9 +29,20 @@ config.color_12 = '#6a9fb5'
 config.color_13 = '#aa759f'
 config.color_14 = '#75b5aa'
 config.color_15 = '#f5f5f5'
-config.color_16 = '#d28445'
-config.color_17 = '#8f5536'
-config.color_18 = '#202020'
-config.color_19 = '#303030'
-config.color_20 = '#b0b0b0'
-config.color_21 = '#e0e0e0'
+
+-- color_0  : black (background)
+-- color_1  : red
+-- color_2  : green
+-- color_3  : brown
+-- color_4  : blue
+-- color_5  : purple
+-- color_6  : cyan
+-- color_7  : light gray (foreground)
+-- color_8  : gray
+-- color_9  : light red
+-- color_10 : light green
+-- color_11 : yellow
+-- color_12 : light blue
+-- color_13 : pink
+-- color_14 : light cyan
+-- color_15 : white
