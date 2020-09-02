dcfa296764d27b1165ca2b9615a41e20539f1ba1
endaaman <buhibuhidog@gmail.com>
2020-09-02 14:41:49 +0900

    update

---
+++ misc/jupyter-vim-bind.js
@@ -0,0 +1,31 @@
+// Configure CodeMirror Keymap
+require([
+  'nbextensions/vim_binding/vim_binding',   // depends your installation
+], function() {
+  // Map jj to <Esc>
+  // CodeMirror.Vim.map("<Ctrl>l", "<End>", "insert");
+  CodeMirror.Vim.map("<Ctrl>l", "<End>", "normal");
+  CodeMirror.Vim.map("<Ctrl>h", "<Home>", "normal");
+
+  // Swap j/k and gj/gk (Note that <Plug> mappings)
+  CodeMirror.Vim.map("<Ctrl>h", "<Backspace>", "insert");
+  // CodeMirror.Vim.map("j", "<Plug>(vim-binding-gj)", "normal");
+  // CodeMirror.Vim.map("k", "<Plug>(vim-binding-gk)", "normal");
+  // CodeMirror.Vim.map("gj", "<Plug>(vim-binding-j)", "normal");
+  // CodeMirror.Vim.map("gk", "<Plug>(vim-binding-k)", "normal");
+});
+
+// Configure Jupyter Keymap
+require([
+  'nbextensions/vim_binding/vim_binding',
+  'base/js/namespace',
+], function(vim_binding, ns) {
+  // Add post callback
+  vim_binding.on_ready_callbacks.push(function(){
+    var km = ns.keyboard_manager;
+    // Allow Ctrl-2 to change the cell mode into Markdown in Vim normal mode
+    km.edit_shortcuts.add_shortcut('ctrl-2', 'vim-binding:change-cell-to-markdown', true);
+    // Update Help
+    km.edit_shortcuts.events.trigger('rebuild.QuickHelp');
+  });
+});
