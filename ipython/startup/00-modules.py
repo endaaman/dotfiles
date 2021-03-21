fd8265fc7fcd002eb9dc13dd148c482cd7ebef8f
endaaman <buhibuhidog@gmail.com>
2021-03-21 14:25:03 +0900

    update

--- ipython/startup/00-modules.py
+++ ipython/startup/00-modules.py
@@ -5,3 +5,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import torch
 import torch.nn as nn
+
+# %config IPCompleter.use_jedi = False
+# ipython profile create
+# edit /home/ken/.ipython/profile_default/ipython_config.py
