f7315c1c6139d2946a52bcc3404fef0c45eae2ad
endaaman <endaaman@frontier.hokudai.ac.jp>
2021-06-22 15:37:53 +0900

    update

--- ipython/startup/00-modules.py
+++ ipython/startup/00-modules.py
@@ -6,6 +6,9 @@ import torch
 import torch.nn as nn
 import pandas as pd
 
+get_ipython().run_line_magic('load_ext', 'autoreload')
+# %load_ext autoreload
+
 # %config IPCompleter.use_jedi = False
 # ipython profile create
 # edit /home/ken/.ipython/profile_default/ipython_config.py
