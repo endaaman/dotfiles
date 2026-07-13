bc2ec6152c74df5b5ec44088533897e33aac972f
endaaman <ken@endaaman.com>
2026-07-13 14:33:17 +0900

    Update lazy

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -240,7 +240,7 @@ return {
   },
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
+    branch = 'master',
     dependencies = { 'nvim-lua/plenary.nvim', },
     config = config,
   },
